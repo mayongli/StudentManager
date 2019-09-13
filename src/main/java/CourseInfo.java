f9a02383fa756b0d2a0b64e71d2e98f45a806577
mayongli <55264810+mayongli@users.noreply.github.com>
2019-09-13 10:34:39 +0800

    Student

---
+++ src/main/java/CourseInfo.java
@@ -0,0 +1,20 @@
+public class CourseInfo {
+    private int courseId;
+    private String courseName;
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
